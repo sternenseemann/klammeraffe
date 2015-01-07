1b1d7aaa7c78dd18c26aed025e776037a5c63166
lukasepple <git@lukasepple.de>
2015-01-07 17:49:03 +0100

    Indentation cleanup

--- Main.hs
+++ Main.hs
@@ -10,39 +10,36 @@ import Network.SimpleIRC
 botNick :: String
 botNick = "klammeraffe"
 freenode = (mkDefaultConfig "irc.freenode.net" botNick)
-			{ cChannels = ["#augsburg"],
-			  cEvents   = [(Privmsg onPrivmsg)]
-			}
+           { cChannels = ["#augsburg"],
+               cEvents   = [(Privmsg onPrivmsg)]
+           }
+
 
 generateClosingParens :: B.ByteString -> B.ByteString
 generateClosingParens msg = E.encodeUtf8 (calcParens (E.decodeUtf8 msg) T.empty)
-	where
-		calcParens :: T.Text -> T.Text -> T.Text
-		calcParens msg parenStack
-			| T.null parenStack && T.null msg   = ""
-			| T.null msg                        = T.cons (T.head parenStack) (calcParens msg (T.tail parenStack))
-			| T.head msg `elem` openingParens = calcParens (T.tail msg) (T.cons (convertToClosingParen (T.head msg)) parenStack)
-			| not (T.null parenStack) &&
-				T.head msg == T.head parenStack = calcParens (T.tail msg) (T.tail parenStack)
-			| T.head msg `elem` closingParens = ""
-			| otherwise                         = calcParens (T.tail msg) parenStack
-			where
-				openingParens = "([<{⟦⟨⟪〚⁅〈⎴⏞⏠❬❰❲❴⦃⦗⧼⸦〈《【〔〖〘"
-				closingParens = ")]>}⟧⟩⟫〛⁆〉⎵⏟⏡❭❱❳❵⦄⦘⧽⸧〉》】〕〗〙"
-				convertToClosingParen p = T.index closingParens (fromMaybe 0 (T.findIndex (== p) openingParens))
-				elem :: Char -> T.Text -> Bool
-				elem c str = isJust (T.findIndex (== c) str)
+  where calcParens :: T.Text -> T.Text -> T.Text
+        calcParens msg parenStack
+          | T.null parenStack && T.null msg   = ""
+          | T.null msg                        = T.cons (T.head parenStack) (calcParens msg (T.tail parenStack))
+          | T.head msg `elem` openingParens   = calcParens (T.tail msg) (T.cons (convertToClosingParen (T.head msg)) parenStack)
+          | not (T.null parenStack) &&
+            T.head msg == T.head parenStack = calcParens (T.tail msg) (T.tail parenStack)
+          | T.head msg `elem` closingParens   = ""
+          | otherwise                         = calcParens (T.tail msg) parenStack
+          where openingParens = "([<{⟦⟨⟪〚⁅〈⎴⏞⏠❬❰❲❴⦃⦗⧼⸦〈《【〔〖〘"
+                closingParens = ")]>}⟧⟩⟫〛⁆〉⎵⏟⏡❭❱❳❵⦄⦘⧽⸧〉》】〕〗〙"
+                convertToClosingParen p = T.index closingParens (fromMaybe 0 (T.findIndex (== p) openingParens))
+                elem :: Char -> T.Text -> Bool
+                elem c str = isJust (T.findIndex (== c) str)
 
 onPrivmsg :: EventFunc
 onPrivmsg server msg 
-	| not (B.null reply) = sendMsg server chan reply
-	| B.isInfixOf (B.pack botNick) (mMsg msg) = sendMsg server chan (B.append nick
-		": I only fix your unclosed parens.")
-	| otherwise = print msg
-	where
-		nick  = fromJust $ mNick msg
-		chan  = fromJust $ mChan msg
-		reply = generateClosingParens (mMsg msg)
+  | not (B.null reply) = sendMsg server chan reply
+  | B.isInfixOf (B.pack botNick) (mMsg msg) = sendMsg server chan (B.append nick ": I only fix your unclosed parens.")
+  | otherwise = print msg
+  where nick  = fromJust $ mNick msg
+        chan  = fromJust $ mChan msg
+        reply = generateClosingParens (mMsg msg)
 
 
 main = connect freenode False True
